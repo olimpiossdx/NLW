0516a50335a497002af122053f122e46db448283
Olimpio Pimenta <olimpiodx@gmail.com>
2020-06-07 00:35:10 -0300

    adicionando versão mobile

---
+++ mobile/src/pages/Points/interfaces/IPoint.ts
@@ -0,0 +1,12 @@
+export default interface IPoint {
+  id: number;
+  image: string;
+  image_url: string;
+  name: string;
+  email: string;
+  whatsapp: string;
+  latitude: number;
+  longitude: number;
+  city: string,
+  uf: string;
+};
